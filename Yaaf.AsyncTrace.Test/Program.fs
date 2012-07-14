33ffa12d37f6052858cbcb2ca5c35d8de63a26a0
Matthias Dittrich <matthi.d@googlemail.com>
2012-07-14 10:38:39 +0200

    Removed Reflection to be compatible with mono, added test project

---
+++ Yaaf.AsyncTrace.Test/Program.fs
@@ -0,0 +1,40 @@
+﻿// ----------------------------------------------------------------------------
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+// ----------------------------------------------------------------------------
+
+open Yaaf.AsyncTrace
+
+let doSomeThingInner v = asyncTrace() {
+    let! (tracer:ITracer) = AsyncTrace.TraceInfo()
+    tracer.logCrit "CRITICAL! %s" v
+    return "ToOuter" }
+
+let testIt () = asyncTrace() {
+    let! (tracer:ITracer) = traceInfo()
+    tracer.logVerb "Verbose!" 
+    let! d = doSomeThingInner "ToInner"
+    tracer.logWarn "WARNING: %s" d }
+    
+// Using it right => Set the tracer before invoking
+let workFlow = testIt()
+// use (Logging.MySource "Yaaf.AsyncTrace" "Name") instead of (Logging.Source "Yaaf.AsyncTrace") 
+// if you have the same workflow running multiple times in parallel
+// you get then files added with the name 
+// For example if you configure logs/logfile.xml in app.config
+// you will then get multiple files with the name logs/logfile.name.xml
+workFlow 
+    |> AsyncTrace.SetTracer (Logging.DefaultTracer (Logging.Source "Yaaf.AsyncTrace") "Workflow 01")
+    |> Async.RunSynchronously
+
+// Using it right => Set the tracer before invoking
+let workFlow2 = testIt()
+// use (Logging.MySource "Yaaf.AsyncTrace" "Name") instead of (Logging.Source "Yaaf.AsyncTrace") 
+// if you have the same workflow running multiple times in parallel
+// you get then files added with the name 
+// For example if you configure logs/logfile.xml in app.config
+// you will then get multiple files with the name logs/logfile.name.xml
+workFlow2
+    |> AsyncTrace.SetTracer (Logging.DefaultTracer (Logging.MySource "Yaaf.AsyncTrace.Test" "Test") "Workflow 01")
+    |> Async.RunSynchronously
+
--- Yaaf.AsyncTrace/Logging.fs
+++ Yaaf.AsyncTrace/Logging.fs
@@ -15,6 +15,52 @@ module LogInterface =
         abstract member TraceSource : TraceSource
         abstract member ActivityId : Guid
 
+type IMyListener = 
+    abstract member Duplicate : string -> TraceListener
+
+module CopyListenerHelper = 
+    let copyListener (fromListener:TraceListener) (toListener:TraceListener) = 
+        toListener.Attributes.Clear()
+        for pair in 
+            fromListener.Attributes.Keys
+                |> Seq.cast
+                |> Seq.map2 (fun k v -> k,v) (fromListener.Attributes.Values |> Seq.cast) do
+            toListener.Attributes.Add pair
+        toListener.Filter <- fromListener.Filter
+        toListener.IndentLevel <- fromListener.IndentLevel
+        toListener.Name <- fromListener.Name
+        toListener.TraceOutputOptions <- fromListener.TraceOutputOptions
+        toListener
+
+    let createNewFilename oldRelFilePath name =
+        if System.String.IsNullOrEmpty(oldRelFilePath) then null
+        else
+            let fileName = Path.GetFileNameWithoutExtension(oldRelFilePath)
+            let extension = Path.GetExtension(oldRelFilePath)
+            Path.Combine(
+                Path.GetDirectoryName(oldRelFilePath),
+                sprintf "%s.%s%s" fileName name extension)
+
+type MyDefaultTraceListener(initData:string, name:string) = 
+    inherit DefaultTraceListener()
+    new() = new MyDefaultTraceListener(null,null)
+    new(s) = new MyDefaultTraceListener(s,s)
+    interface IMyListener with
+        member x.Duplicate name =
+            CopyListenerHelper.copyListener
+                x
+                (new DefaultTraceListener() :> TraceListener)
+
+type MyXmlWriterTraceListener(initData:string, name:string) = 
+    inherit XmlWriterTraceListener(initData)
+    new(s) = new MyXmlWriterTraceListener(s,s)
+    interface IMyListener with
+        member x.Duplicate name =
+            let newPath = CopyListenerHelper.createNewFilename initData name
+            CopyListenerHelper.copyListener 
+                x
+                (new MyXmlWriterTraceListener(newPath) :> TraceListener)
+
 [<AutoOpen>]
 module LoggingModule = 
     type ITracer with 
@@ -42,50 +88,20 @@ module LoggingModule =
         do 
             // NOTE: Shared Listeners are not supported
             // (currently we create new ones and do not share them the same way)
-            let flags = System.Reflection.BindingFlags.NonPublic ||| 
-                                                System.Reflection.BindingFlags.Instance
+            let flags = System.Reflection.BindingFlags.Public |||
+                        System.Reflection.BindingFlags.NonPublic ||| 
+                        System.Reflection.BindingFlags.Instance
+                        
+            let eventCache = new TraceEventCache(); 
+
             let newTracers = [|
                 for l in x.Listeners do
-                    let t = l.GetType()
-                    let initField =
-                        t.GetField(
-                            "initializeData", flags)
-                    let oldRelFilePath =
-                        if initField <> null then
-                                initField.GetValue(l) :?> string
-                        else System.IO.Path.Combine("logs", sprintf "%s.log" l.Name)
-                    
-                    let newFileName =
-                        if System.String.IsNullOrEmpty(oldRelFilePath) then null
-                        else
-                            let fileName = Path.GetFileNameWithoutExtension(oldRelFilePath)
-                            let extension = Path.GetExtension(oldRelFilePath)
-                            Path.Combine(
-                                Path.GetDirectoryName(oldRelFilePath),
-                                sprintf "%s.%s%s" fileName name extension)
-
-                    // NOTE: On mono the DefaultTraceSource Listener Constructor is private
-                    let constr = 
-                        t.GetConstructor(
-                            flags, 
-                            null, 
-                            (if newFileName = null then [| |] else [| typeof<string> |]), 
-                            null)
-                    if (constr = null) then 
-                        failwith (sprintf "TraceListener Constructor for Type %s not found" (t.FullName))
-                    let listener = constr.Invoke(if newFileName = null then [| |]  else [| newFileName |]) :?> TraceListener
-                    // Copy other properties.
-                    listener.Attributes.Clear()
-                    for pair in 
-                        l.Attributes.Keys
-                            |> Seq.cast
-                            |> Seq.map2 (fun k v -> k,v) (l.Attributes.Values |> Seq.cast) do
-                        listener.Attributes.Add pair
-                    listener.Filter <- l.Filter
-                    listener.IndentLevel <- l.IndentLevel
-                    listener.Name <- l.Name
-                    listener.TraceOutputOptions <- l.TraceOutputOptions
-                    yield listener |]
+                    match l:>obj with
+                    | :? IMyListener as myListener ->
+                        yield myListener.Duplicate(name)
+                    | _ ->
+                        l.TraceEvent(eventCache, x.Name, TraceEventType.Error, 0, sprintf "Unknown Listener, can't apply name \"%s\"" name)
+                |]
             x.Listeners.Clear()
             x.Listeners.AddRange(newTracers)
 
